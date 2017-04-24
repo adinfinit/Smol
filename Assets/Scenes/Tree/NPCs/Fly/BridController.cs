e6cf58e66c9f4e2d5fd65661f482122152369ecc
Egon Elbre <egonelbre@gmail.com>
2017-04-24 16:08:25 +0300

    Add Fly and Scenery.

---
+++ Assets/Scenes/Tree/NPCs/Fly/BridController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridController : MonoBehaviour {
+
+    public bool isTriggered = false;
+    public float speed = 10f;
+
+    private Vector3 direction;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = this.gameObject.GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isTriggered)
+        {
+            // do burd stuff 
+            this.rb.position +=  Time.deltaTime * ((speed * direction) +
+                new Vector3(4.1f * Mathf.Sin(3f * Time.time) + 1.3f * Mathf.Sin(1f * Time.time),
+                            1.4f * Mathf.Sin(4.3f * Time.time),
+                            4.25f * Mathf.Sin(Time.time * 0.1f) + 3.7f * Mathf.Sin(Time.time * 1.5f)));
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, (direction - new Vector3(0f, 0.85f, 0f)), 3f * Time.deltaTime, 0.0F);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+            transform.rotation = Quaternion.LookRotation(newDir);
+
+        }
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            isTriggered = true;
+            rb.useGravity = false;
+
+            direction = (this.rb.position - other.transform.position);
+            direction = new Vector3(direction.x, 0f, direction.z).normalized;
+            direction = new Vector3(direction.x, 1f, direction.z);
+        }
+    }
+}
---
+++ Assets/Sound/SoundManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+	[Header("General attributes")]
+	public AnimationCurve fadeCurve;
+	[Range(0.001f, 10f)] public float fadeTime = 10f;
+	[Range(0f, 1f)] public float maxVolume = 1f;
+
+    [Header("Playlist")]
+    public AudioClip[] playlist;
+    private AudioClip[] randomizedPlaylist; 
+
+    [Header("Mixer")]
+    public AudioMixerGroup mixer;
+
+    private int curIdx = 0;
+	private AudioClip inSound;
+	private AudioSource mainSound;
+	private AudioSource secondarySound;
+	private bool fading;
+	private float startedFadingAt;
+
+
+	// Use this for initialization
+	void Start () {
+		mainSound = gameObject.AddComponent<AudioSource>();
+		secondarySound = gameObject.AddComponent<AudioSource> ();
+
+        mainSound.outputAudioMixerGroup = mixer;
+        secondarySound.outputAudioMixerGroup = mixer;
+
+		mainSound.Play ();
+		secondarySound.Play ();
+
+		fading = false;
+
+		// checks every second if the mainSound is behaving as it should
+		InvokeRepeating ("clampVolume", 0f, 1f);
+
+        curIdx = Random.Range(0, playlist.Length - 1);
+        PlayRandomMusicIndefinitely();
+
+	}
+
+    void RandomizeList(AudioClip[] list)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		Fade ();
+	}
+
+	private void Fade() {
+
+		// if we are not currently fading
+		if (!fading) {
+			// if we get a fade request
+			if (inSound != null) {
+				startedFadingAt = Time.time; // save the time we started fading
+				secondarySound.clip = inSound; // set the clip to secondary audio player
+				secondarySound.time = 0f;
+				secondarySound.Play ();
+				inSound = null; // reset the in sound to null so we wont do it again
+				fading = true;
+			}
+
+		} else {
+			float curveTime = (Time.time - startedFadingAt) / fadeTime;
+			float curveValue = fadeCurve.Evaluate (curveTime);
+
+			mainSound.volume = (1f - curveValue) * maxVolume;
+			secondarySound.volume = (curveValue) * maxVolume;
+
+			// if we have finished our fading  
+			if (curveTime >= 1f) {
+				fading = false;
+
+				// switch mainSound and secondarySound
+				mainSound.clip = secondarySound.clip;
+				mainSound.volume = maxVolume;
+				mainSound.Play ();
+				mainSound.time = secondarySound.time;
+
+				secondarySound.clip = null;
+				secondarySound.volume = 0f;
+				secondarySound.time = 0f;
+			}
+		}
+	}
+
+	private void clampVolume() {
+		mainSound.volume = Mathf.Min (maxVolume, mainSound.volume);
+	}
+
+	// Use this function from outside to fade another track in 
+	public void MakeFade(AudioClip inMusic) {
+		inSound = inMusic;
+	}
+
+    public void PlayRandomMusicIndefinitely()
+    {
+        // pick next
+        AudioClip luckyOne = playlist[curIdx];
+        curIdx = (curIdx + 1) % (playlist.Length - 1);
+
+        this.MakeFade(luckyOne);
+
+        Invoke("PlayRandomMusicIndefinitely", this.inSound.length - this.fadeTime);
+    }
+}
+
