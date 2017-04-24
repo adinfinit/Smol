05944c20ef6c59dfe732f572010c340f2d2e105b
Egon Elbre <egonelbre@gmail.com>
2017-04-24 16:36:52 +0300

    Add Menu.

---
+++ Assets/Scenes/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+	public void LoadMenu ()
+	{
+		StaticLoadMenu ();
+	}
+
+	public void LoadTutorial ()
+	{
+		StaticLoadTutorial ();
+	}
+
+	public void LoadPlay ()
+	{
+		StaticLoadPlay ();	
+	}
+
+	public static void StaticLoadMenu ()
+	{
+		SceneManager.LoadScene ("Scenes/Menu/MenuScene");
+	}
+
+	public static void StaticLoadTutorial ()
+	{
+		SceneManager.LoadScene ("Scenes/Tutorial/TutorialScene");
+	}
+
+	public static void StaticLoadPlay ()
+	{
+		SceneManager.LoadScene ("Scenes/Tree/TreeScene");
+	}
+}
