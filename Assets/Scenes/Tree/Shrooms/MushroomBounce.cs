bd928b6fed6a7fbc10afbb28e91b831399d9b003
Egon Elbre <egonelbre@gmail.com>
2017-04-24 14:17:57 +0300

    Update terrain and tree.

---
+++ Assets/Scenes/Tree/Shrooms/MushroomBounce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomBounce : MonoBehaviour
+{
+
+	private GameObject player;
+	private DashController controller;
+	private Rigidbody rb;
+
+	public float magnitude = 10f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		controller = player.GetComponent<DashController> ();
+		rb = player.GetComponent<Rigidbody> ();       
+	}
+
+	void Update ()
+	{
+		Debug.DrawRay (this.transform.position, Vector3.up * 10f, Color.red);
+		Debug.DrawRay (this.transform.position, transform.rotation * Vector3.up * 3f, Color.black);
+		Debug.DrawRay (this.transform.position, transform.up * 3f, Color.blue);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player") {
+			controller.ExternalDash (transform.up * magnitude);           
+		}
+	}
+}
